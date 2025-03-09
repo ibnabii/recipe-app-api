d22cceb7db705cf0bdd500b70f13f034c6e8486c
ibnabii <someone@somewhere.net>
2025-03-09 19:56:11 +0100

    Dockerized postggresql

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,38 @@
+"""
+Test custom Django management commands.
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch("core.management.commands.wait_for_db.Command.check")
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready"""
+        patched_check.return_value = True
+        call_command("wait_for_db")
+        patched_check.assert_called_once_with(databases=["default"])
+
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting  OperationalError"""
+
+        # side_effect exception = mocking library raises the exception
+        # here 2 times we rise Psycopg2Error,
+        # then 3 times OperationalError, and finally True
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
+
+        call_command("wait_for_db")
+        # method should be called only 6 times, cause we expect True after 6th
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=["default"])
