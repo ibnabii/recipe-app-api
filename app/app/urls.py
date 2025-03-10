99cff0e778bb37b7cba3c3f13b64a38dc31103a4
ibnabii <someone@somewhere.net>
2025-03-10 11:35:45 +0100

    User registration

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,6 +1,6 @@
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("user.urls")),
 ]
