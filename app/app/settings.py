af04f10760197b5634d4840b5b537068dd0c4008
ibnabii <someone@somewhere.net>
2025-03-11 12:11:43 +0100

    Create token API

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t!4qjzq(5v8ytt7g2ycxqn+6^0!hu%0mm%f6n(idv(+0@vq8uy"
+SECRET_KEY = (
+    "django-insecure-t!4qjzq(5v8ytt7g2ycxqn+6^0!hu%0mm%f6n(idv(+0@vq8uy"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "rest_framework",
+    "rest_framework.authtoken",
     "drf_spectacular",
     "user",
 ]
