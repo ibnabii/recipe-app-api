6e5f1609177185c27d77199f796c563a93e216cd
ibnabii <someone@somewhere.net>
2025-03-12 09:14:13 +0100

    Recipe API

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,6 +4,8 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+admin.site.register(models.Recipe)
+
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
